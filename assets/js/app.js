8c3cf4469d050f3936e993bc433968137dd8910c
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-18 16:38:09 -0700

    updating homework

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,13 @@ var svgHeight = 500;
 var margin = {
     top: 20,
     right: 40,
-    bottom: 60,
+    bottom: 150,
     left: 100
 }
 
 //define dimensions of the chart area
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 
 //select scatter id from index.html, append SVG area to it, and set the dimensions
 var svg = d3.select("#scatter")
@@ -26,7 +26,7 @@ var svg = d3.select("#scatter")
     .attr("height", svgHeight);
 
 //append a group to the svg area and shift ('translate')  it to the right and down to adhere
-//to the martins set in the chartMargin object   
+//to the margins set in the chartMargin object   
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
@@ -40,8 +40,76 @@ d3.csv("assets/data/data.csv").then (function(censusData){
     //need to convert poverty and healthcareLow into numerical values
     //===============================================================
     censusData.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.healthcareLow = +data.healthcareLow;
 
     });
+
+    //step 2:  create scale function for x and y axis
+    //===================================================
+    //domain is original data.  range the domain scaled in terms of pixels
+
+    //create a linear scale for the x axis
+    var xLinearScale = d3.scaleLinear()
+    // .domain([0, d3.max(censusData, d=>d.poverty)])
+    // .domain([d3.extent(censusData, d => d.poverty)])
+    .domain([7, d3.max(censusData, d=>d.poverty)])
+    .range([0, chartWidth]);
+    
+    //create a linear scale for the y axis
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(censusData, d=>d.healthcareLow)])
+    .range([chartHeight, 0]);
+
+
+    //step 3:  create axis functions
+    //==================================================
+    //instantiate x and y axis
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    //step 4:  append axis to chart
+    //==================================================
+    chartGroup.append("g")
+    .call(leftAxis);
+
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+
+    //step 5:  Create circles
+    //==================================================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcareLow))
+    .attr("r", "15")
+    .attr("fill", "blue")
+    .attr("opacity", ".5")
+    //append text
+    .append("text")
+    .text(function(d) {
+        return d.state;});
+
+
+    // .attr("dy", ".3em");
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (chartHeight / 2) -60)
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare(%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
+
 
 
 }).catch(function(error) {
